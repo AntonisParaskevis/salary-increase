e05de9372c9c6ebdea568f5accb59e477439d318
Antonis Paraskevis <47597258+AntonisParaskevis@users.noreply.github.com>
2025-10-28 20:19:51 +0200

    Add files via upload

---
+++ salary_increase.py
@@ -0,0 +1,35 @@
+while True:
+    try:
+        # Prompt the user to enter the current salary
+        salary = float(input("Enter the current salary\n"))
+        
+        # If the user has entered either zero or a negative number, prompt them to enter a valid input
+        if salary <= 0:
+            print("Invalid entry, please enter a number greater than zero.")
+            continue
+        break
+    except ValueError:
+        # If the user has entered a non-numerical input, prompt them to enter a valid input
+        print("Invalid entry, please enter a number greater than zero.")
+
+# Store the salary in the original_salary variable to save it for later use during the program's execution
+original_salary = salary
+
+# Increase the salary by 5% 3 times
+salary = salary + salary * 0.05
+
+salary = salary + salary * 0.05
+
+salary = salary + salary * 0.05
+
+# Calculate the salary change percentage
+change_percentage = ((salary - original_salary) / original_salary) * 100
+
+# Display the new salary, rounded to 2 decimals
+print("New Salary: $" + str(round(salary, 2)) + ".")
+
+# Display the salary change percentage, rounded to 2 decimals
+print("Salary change percentage: " + str(round(change_percentage, 2)) + "%.")
+
+# In the end, prompt the user to press Enter, in order to exit the program
+input("Press Enter to exit the program")
